4478331da989aaab443058622929dd7042e13114
Frédéric Chapoton <chapoton@unistra.fr>
2019-03-14 16:13:59 +0100

    first steps towards package creation (setup.py and README.md)

---
+++ README.md
@@ -0,0 +1,19 @@
+# Floer
+
+This is a python package calculating the knot Floer Homology.
+
+This still experimental python program calculates the knot Floer
+homology of a knot. The program is started by executing main.py, its
+main output is an array containing for each Alexander and Maslov
+grading the rank of the homology group.
+
+The running time of the program is sometimes huge. Further faster
+versions should appear. The program is based on the work of Stephen
+J. Bigelow and Anna Beliakova, itself prolongating the now classical
+combinatorial description of knot Floer homology.
+
+The author of the initial code is Jean-Marie Droz.
+
+See https://user.math.uzh.ch/droz/
+
+This works with both Python2 and Python3.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='Floer',
+      version='0.1',
+      description='computation of Floer homology groups of knots',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Jean-Marie Droz',
+      license='GPL',
+      packages=['Floer'],
+      entry_points={
+          'console_scripts': ['Floer=Floer:main']},
+      package_data={
+          'Floer': ['knotAtlasV1.txt', 'knotAtlas.pic']
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3"
+          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+          "Operating System :: OS Independent",
+          "Topic :: Scientific/Engineering :: Mathematics",
+      ],
+      zip_safe=False)
